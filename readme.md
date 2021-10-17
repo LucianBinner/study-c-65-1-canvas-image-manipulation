639c527e29ae3043a1c2951e941560095ebbcbc4
Binner <lucian.binner@hotmail.com>
2021-10-16 23:04:45 -0300

    feat: create selection tool

--- readme.md
+++ readme.md
@@ -5,9 +5,27 @@
 
 ## Select & Preview image
 * [x] Criar o arquivo script.js
-* [x] Adicionar evento 'click' no elemento #select-image, que irá executar a funcionalidade de clicar no elemento #foto-file
+* [x] Adicionar evento 'click' no elemento #select-image, que irá executar a funcionalidade de clicar no elemento #photo-file
 * [x] Ao terminar o evento de carregar a DOM
-* [x] Adicionar evento 'change' no elemento #foto-file
+* [x] Adicionar evento 'change' no elemento #photo-file
 * [x] Criar im novo FileReader()
 * [x] Usar a funcionalidade .readAsDataURL() para leitura do arquivo
-* [x] Ao terminar de ler o arquivo, adicionar o src do elemento img#foto-preview
+* [x] Ao terminar de ler o arquivo, adicionar o src do elemento img#photo-preview
+
+## Selection tool
+* [x] Criar elmento #selection-tool no html
+* [x] Estilizar o elemento
+* [x] No JS, criar eventos do mouse
+    * [x] mouseover, mousedown, mousemove, mouseup
+* [x] Registrar eventos
+* [x] Adicionar funcionalidade aos eventos
+    * [x] mouseover: crosshair no cursor
+    * [x] mousedown:
+        * [x] Registrar posicionamentos iniciais de X e Y (absolutos e relativos)
+        * [x] Adicionar flag de inicio de seleção
+    * [x] mousemove:
+        * [x] Registrar posicionamentos finais de X e Y
+        * [x] Atualizar estilos visuais da seleção
+    * [x] mouseup:
+        * [x] Registrar posicionamento final (relativo) do X e Y
+        * [x] Remover flag de inicio de seleção
--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 const photoFile = document.getElementById('photo-file')
+const image = document.getElementById('photo-preview')
 
+// Select and preview image
 document
-    .getElementById('select-image')
-    .onclick = () => {
-        photoFile.click()
-    }
+  .getElementById('select-image')
+  .onclick = () => {
+    photoFile.click()
+  }
 
 window.addEventListener('DOMContentLoaded', () => { // Executar depois que a DOM é carregada
-    photoFile.addEventListener('change', () => {
-        let file = photoFile.files.item(0) // Pegando a foto carregada 
-        let reader = new FileReader()  // Leito de arquivo no HTML
-        reader.readAsDataURL(file)
-        reader.onload = (event) => {
-            let image = document.getElementById('photo-preview')
-            image.src = event.target.result
-        }
-    })
+  photoFile.addEventListener('change', () => {
+    const file = photoFile.files.item(0) // Pegando a foto carregada 
+    const reader = new FileReader()  // Leito de arquivo no HTML
+    reader.readAsDataURL(file)
+    reader.onload = (event) => {
+      image.src = event.target.result // Atribui o target da imagem carregada ao elemento image
+    }
+  })
+})
+
+// Selection tool
+const selection = document.getElementById('selection-tool')
+let startX, startY, relativeStartX, relativeStartY, endX, endY, relativeEndX, relativeEndY
+let startSelection = false
+const events = {
+  mouseover() {
+    // this neste contexto é quem esta disparando o evento ou seja o image
+    this.style.cursor = 'crosshair'
+  },
+  mousedown() {
+    const { clientX, clientY, offsetX, offsetY } = event // Pega os eventos ocorridos
+    startX = clientX
+    startY = clientY
+    relativeStartX = offsetX
+    relativeStartY = offsetY
+    startSelection = true
+  },
+  mousemove() {
+    const { clientX, clientY } = event
+    endX = clientX
+    endY = clientY
+    if (startSelection) {
+      selection.style.display = 'initial'
+      selection.style.top = startY + 'px'
+      selection.style.left = startX + 'px'
+      selection.style.width = (endX - startX) + 'px'
+      selection.style.height = (endY - startY) + 'px'
+    }
+  },
+  mouseup() {
+    const { layerX, layerY } = event
+    startSelection = false
+    relativeEndX = layerX
+    relativeEndY = layerY
+  }
+}
+
+Object.keys(events).forEach(eventName => {
+  image.addEventListener(eventName, events[eventName]) // Passa cada um dos eventos de mouse para o elemento image 'photo-preview', forma simplificada para não ficar passando um por um
 })
